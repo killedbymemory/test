e02c55b86c9ead214483ce9dff480940f171ee24
Leonardo Situmorang <leonardo@icehousecorp.com>
2014-02-22 11:28:33 +0700

    original fizzBuzz now also only process positive integer including zero (0) value. more test cases added.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,14 +5,19 @@ function fizzBuzz(input) {
   var _output;
 
   for(var i in input) {
-    if ((input[i] % 3 == 0) && (input[i] % 5 == 0)) {
+    _output = input[i];
+
+    if (input[i] < 0) {
+      // only take care of positive integer
+      continue;
+    } else if (input[i] == 0) {
+      _output = 0;
+    } else if ((input[i] % 3 == 0) && (input[i] % 5 == 0)) {
       _output = 'FizzBuzz';
     } else if (input[i] % 3 == 0) {
       _output = 'Fizz';
     } else if (input[i] % 5 == 0) {
       _output = 'Buzz';
-    } else {
-      _output = input[i];
     }
 
     output.push(_output);
@@ -36,6 +41,8 @@ function assertEqual(condition, test) {
   console.log(output);
 }
 
+assertEqual(fizzBuzz([-5, -4, -3, -2, -1, 0, 1, 2, 3]), '0 1 2 Fizz');
+assertEqual(fizzBuzz([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]), '0 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11');
 assertEqual(fizzBuzz([12, 13, 14, 15, 16]), 'Fizz 13 14 FizzBuzz 16');
 assertEqual(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz');
 assertEqual(fizzBuzz([45, 46, 47, 48, 49, 50]), 'FizzBuzz 46 47 Fizz 49 Buzz');
